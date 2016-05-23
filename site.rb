88dd2a078874950e94373c40b3b13cc09b3768b4
Andy Phillips <skiing105@gmail.com>
2016-05-23 20:55:41 +0000

    Reinitialize

---
+++ site.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+ENV['PORT'] ||= '4000'
+set :port, ENV[ 'PORT']
+
+set :port, 4000
+set :bind, '0.0.0.0'
+get '/' do
+  erb :index
+end
+get '/italian' do
+    "Ciao!"
+end
+get '/read_more' do
+   erb :read_more
+ end
