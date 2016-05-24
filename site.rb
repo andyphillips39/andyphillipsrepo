8e6a60882f333bb60bca64c10f136d7db368a907
Andy Phillips <skiing105@gmail.com>
2016-05-24 00:58:23 +0000

    site.rb fix

--- site.rb
+++ site.rb
@@ -1,9 +1,11 @@
 require 'bundler/setup'
 require 'sinatra'
-ENV['PORT'] ||= '4000'
-set :port, ENV[ 'PORT']
 set :port, 4000
 set :bind, '0.0.0.0'
+
+ENV['PORT'] ||= '4000'
+set :port, ENV[ 'PORT']
+
 get '/' do
   erb :index
 end
